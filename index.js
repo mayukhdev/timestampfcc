43f0cc737ec21d9799d48d7d06230b34a4870afe
Mayukh Dev <mayukhdev@gmail.com>
2016-01-31 00:22:33 +0530

    Completed task with html and js

--- index.js
+++ index.js
@@ -4,13 +4,48 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:data', function(request, response) {	
+	var jso = {
+		"unix" : "",
+		"natural" : ""
+	};
+	var month =  {0:'January', 1:'February', 2:'March', 3:'April', 4:'May', 5:'June', 			6:'July', 7:'August', 8:'September', 9:'October', 10:'November', 11:'December'}
+	var date;
+	if(isNaN(parseInt(request.params.data))){
+		date = new Date(request.params.data);
+	}else{
+		date = new Date(parseInt(request.params.data));
+	}
+  	if ( Object.prototype.toString.call(date) === "[object Date]" ) {
+		if ( isNaN( date.valueOf() ) ) {  // d.valueOf() could also work
+    			jso = {
+			"unix" : "null",
+			"natural" : "null"
+			};
+			response.json(jso);
+  		}
+  		
+		else {
+			jso.unix = date.getTime();
+			jso.natural = month[date.getMonth()] + " " + date.getDate() 
+				+ ", " + date.getFullYear();
+			response.json(jso);
+	    	}
+	}
+	else{
+		jso = {
+			"unix" : "null",
+			"natural" : "null"
+			};
+			response.json(jso);
+	}	
+			   
 });
 
 app.listen(app.get('port'), function() {
